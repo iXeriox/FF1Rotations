12e4cd09efde90621d3b1c86a09637b2e275d28a
Leo <leon.savage@outlook.com>
2026-09-11 20:44:07 +0100

    Log Discord interaction usage

---
+++ src/deploy-commands.js
@@ -0,0 +1,13 @@
+import { REST, Routes } from 'discord.js';
+import { commands } from './commands/index.js';
+import { getConfig } from './config.js';
+
+const config = getConfig({ requireClientId: true });
+const rest = new REST().setToken(config.token);
+const body = commands.map((command) => command.data.toJSON());
+const route = config.guildId
+  ? Routes.applicationGuildCommands(config.clientId, config.guildId)
+  : Routes.applicationCommands(config.clientId);
+
+await rest.put(route, { body });
+console.log(`Deployed ${body.length} commands ${config.guildId ? `to guild ${config.guildId}` : 'globally'}.`);

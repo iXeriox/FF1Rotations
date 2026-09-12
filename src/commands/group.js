fee9cdf0d490e9c80cefca6576652211d065cb67
Leo <leon.savage@outlook.com>
2026-09-12 01:45:24 +0100

    Avoid member timeout when clearing leaders

--- src/commands/group.js
+++ src/commands/group.js
@@ -24,6 +24,7 @@ export default {
       latest.players = [];
       latest.playerQueuedAt = {};
       latest.leaders = [];
+      latest.mockUsers = {};
       latest.waitingOpen = false;
       recordRotation(latest, groups);
     });
